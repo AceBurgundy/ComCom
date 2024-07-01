d5ab36a2a055fc5d72d58b872f197cd9538047e0
AceBurgundy <samadriansabalo99@gmail.com>
2024-07-01 12:43:44 +0800

    Added script to manage message sending and receiving.

--- Engine/room/static/room/reminders.js
+++ Engine/room/static/room/reminders.js
@@ -1,22 +1,73 @@
-import {element} from '../../../static/helper.js';
+import makeToastNotification, {element, isCurrentUser} from '../../../static/helper.js';
 import {socket} from '../../../static/socket.js';
 
-const toggle = element('#main-content-rooms-chat-bottom-right-reminder');
-const alerts = element('#main-content-rooms-chat-bottom-right-alerts');
-const chatList = element('#main-content-rooms-chat-center-right');
+const chatList = element('#main-content-rooms-chat-center');
+const sendButton = element('#main-content-rooms-chat-bottom-right-input-send');
+const messageInput = element('#main-content-rooms-chat-bottom-right-input');
 
-toggle.onclick = () => alerts.classList.toggle('active');
+/**
+ * Inserts new message template into the chat list
+ * @param {object} messageObject - received from flask socket-io 'received-message' route
+ * @param {Integer} messageObject.senderId - The id of the sender
+ * @param {Integer} messageObject.senderUsername - The username of the sender
+ * @param {String} messageObject.text - The content of the message
+ * @returns {Promise<void>}
+ */
+async function createMessage(messageObject) {
+  const isSender = await isCurrentUser(messageObject.senderId);
+  let newMessage = /* html */`
+  <tr class="main-content-room-chat-center__row sender">
+    <td class="main-content-room-chat-center__row__left"></td>
+    <td class="main-content-room-chat-center__row__right">
+        <p class="main-content-room-chat-center__row__right__sender">${ messageObject.senderUsername }</p>
+        <p class="main-content-room-chat-center__row__right__text">${ messageObject.text }</p>
+    </td>
+  </tr>`;
 
-const createMessage = message => chatList.innerHTML = /* html */`<li>${message}</li>` + chatList.innerHTML;
+  if (!isSender) {
+    newMessage = /* html */`
+      <tr class="main-content-room-chat-center__row sender">
+        <td class="main-content-room-chat-center__row__left">
+          <p class="main-content-room-chat-center__row__left__sender">${ messageObject.senderUsername }</p>
+          <p class="main-content-room-chat-center__row__left__text">${ messageObject.text }</p>
+        </td>
+        <td class="main-content-room-chat-center__row__right"></td>
+      </tr>`;
+    }
+  ;
 
-alerts.onclick = event => {
-  const roomCode = event.target.parentElement.dataset.roomCode;
-  const message = event.target.innerHTML;
+  chatList.innerHTML = chatList.innerHTML + newMessage;
+  messageInput.value = '';
+};
 
-  socket.emit('new-alert', {
+// Handles sending messages back to the server
+function sendMessage() {
+  const message = messageInput.value.trim();
+
+  if (message === '') {
+    makeToastNotification('Message cannot be empty');
+    return;
+  }
+
+  const roomCode = sendButton.dataset.roomCode;
+
+  socket.emit('send-message', {
     roomCode,
     message
   });
+}
+
+// Sent using send button
+sendButton.onclick = () => sendMessage();
+
+// Sent using enter button
+messageInput.onkeyup = event => {
+  if (event.key != 'Enter') {
+    return;
+  }
+
+  sendMessage();
 };
 
-socket.on('alert', message => createMessage(message));
+// Displays message
+socket.on('received-message', async message => await createMessage(message));
